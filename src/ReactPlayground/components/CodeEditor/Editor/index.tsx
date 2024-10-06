e25571b48414a1a65482e02e67424dff798768ce
JD <591883039@qq.com>
2024-10-06 15:30:16 +0800

    feat: init react-playground-project

---
+++ src/ReactPlayground/components/CodeEditor/Editor/index.tsx
@@ -0,0 +1,66 @@
+import MonacoEditor, { EditorProps, OnMount } from "@monaco-editor/react";
+import { createATA } from "./ata";
+import { editor } from "monaco-editor";
+
+export interface EditorFile {
+  name: string;
+  value: string;
+  language: string;
+}
+
+interface Props {
+  file: EditorFile;
+  onChange?: EditorProps["onChange"];
+  options?: editor.IStandaloneEditorConstructionOptions;
+}
+
+export default function Editor(prop: Props) {
+  const { file, onChange, options } = prop;
+
+  const handleEditorMount: OnMount = (editor, monaco) => {
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
+      editor.getAction("editor.action.formatDocument")?.run();
+    });
+
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+      jsx: monaco.languages.typescript.JsxEmit.Preserve,
+      esModuleInterop: true,
+    });
+
+    const ata = createATA((code, path) => {
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        code,
+        `file://${path}`
+      );
+    });
+
+    editor.onDidChangeModelContent(() => {
+      ata(editor.getValue());
+    });
+
+    ata(editor.getValue());
+  };
+
+  return (
+    <MonacoEditor
+      height="100%"
+      language={file.language}
+      path={file.name}
+      onMount={handleEditorMount}
+      onChange={onChange}
+      value={file.value}
+      options={{
+        fontSize: 14,
+        scrollBeyondLastLine: false,
+        minimap: {
+          enabled: false,
+        },
+        scrollbar: {
+          verticalScrollbarSize: 6,
+          horizontalScrollbarSize: 6,
+        },
+        ...options,
+      }}
+    />
+  );
+}
