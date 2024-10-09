8abbb96df3c8b1f9d88b61a2f78a75b55d53d97d
JD <591883039@qq.com>
2024-10-09 15:38:25 +0800

    feat: fileNameItem add remove

--- src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
+++ src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
@@ -2,6 +2,11 @@ import { useContext, useEffect, useState } from "react";
 import { PlaygroundContext } from "../../../PlaygroundContext";
 import { FileNameItem } from "./FileNameItem";
 import styles from "./index.module.scss";
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from "../../../files";
 
 export default function FileNameList() {
   const {
@@ -18,6 +23,29 @@ export default function FileNameList() {
     setTabs(Object.keys(files));
   }, [files]);
 
+  const handleEditComplete = (name: string, prevName: string) => {
+    updateFileName(prevName, name);
+    setSelectedFileName(name);
+    setCreating(false);
+  };
+
+  const [creating, setCreating] = useState(false);
+  const addTab = () => {
+    addFile("Comp" + Math.random().toString().slice(2, 8) + ".tsx");
+    setCreating(true);
+  };
+
+  const handleRemove = (name: string) => {
+    removeFile(name);
+    setSelectedFileName(ENTRY_FILE_NAME);
+  };
+
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ];
+
   return (
     <div className={styles.tabs}>
       {tabs.map((tab, index) => (
@@ -25,11 +53,20 @@ export default function FileNameList() {
           <FileNameItem
             key={tab + index}
             value={tab}
+            readonly={readonlyFileNames.includes(tab)}
+            creating={creating && index === tabs.length - 1}
             actived={selectedFileName === tab}
             onClick={() => setSelectedFileName(tab)}
+            onEditComplete={(name: string) => handleEditComplete(name, tab)}
+            onRemove={() => {
+              handleRemove(tab);
+            }}
           ></FileNameItem>
         </div>
       ))}
+      <div className={styles.add} onClick={addTab}>
+        +
+      </div>
     </div>
   );
 }
