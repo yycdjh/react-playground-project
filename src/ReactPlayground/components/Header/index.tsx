9a0c5bd1a964505b5723ccabdeac20a614d8f17d
JD <591883039@qq.com>
2024-10-14 16:31:26 +0800

    feat: header add share download

--- src/ReactPlayground/components/Header/index.tsx
+++ src/ReactPlayground/components/Header/index.tsx
@@ -2,10 +2,18 @@ import styles from "./index.module.scss";
 import logoSvg from "./icons/logo.svg";
 import { useContext } from "react";
 import { PlaygroundContext } from "../../PlaygroundContext";
-import { MoonOutlined, SunOutlined } from "@ant-design/icons";
+import {
+  DownloadOutlined,
+  MoonOutlined,
+  ShareAltOutlined,
+  SunOutlined,
+} from "@ant-design/icons";
+import { message } from "antd";
+import copy from "copy-to-clipboard";
+import { downloadFiles } from "../../utils";
 
 export default function Header() {
-  const { theme, setTheme } = useContext(PlaygroundContext);
+  const { files, theme, setTheme } = useContext(PlaygroundContext);
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -27,6 +35,20 @@ export default function Header() {
             onClick={() => setTheme("light")}
           />
         )}
+        <ShareAltOutlined
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            copy(window.location.href);
+            message.success("分享成功");
+          }}
+        ></ShareAltOutlined>
+        <DownloadOutlined
+          style={{ marginLeft: "10px" }}
+          onClick={async () => {
+            await downloadFiles(files);
+            message.success("下载成功");
+          }}
+        ></DownloadOutlined>
       </div>
     </div>
   );
